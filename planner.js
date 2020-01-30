9a3c21dcca75549335e6e1adf7eea22ab2c8cfd4
Christian Sanchez <cas81695@gmail.com>
2020-01-30 11:44:20 -0500

    planner.jss addition of the local storage and JSON.parse for each time slot

--- planner.js
+++ planner.js
@@ -7,7 +7,7 @@ $("#currentDate").text(m.format("dddd, MMMM Do"));
 
 $(document).ready(function() {
     selectHour ();
-    renderText ();
+    inputText ();
 
 });
 
@@ -32,3 +32,52 @@ function selectHour () {
             }
         });
     }
+
+    $(".saveButton").click(function() {
+        words = $(this).sibilings(".input").val();
+        hourInfo = $(this).sibilings(".input").val();
+        localStorage.setItem(hourInfo, JSON.stringify(words));
+        
+        selectHour ();
+        inputText ();
+    
+    })
+
+    function inputText () {
+        var saveInput9 = JSON.parse(localStorage.getItem("9 AM"));
+        $("9").val("");
+        $("9").val(saveInput9);
+
+        var saveInput10 = JSON.parse(localStorage.getItem("10 AM"));
+        $("10").val("");
+        $("10").val(saveInput10);
+
+        var saveInput11 = JSON.parse(localStorage.getItem("11 AM"));
+        $("11").val("");
+        $("11").val(saveInput11);
+
+        var saveInput12 = JSON.parse(localStorage.getItem("12 PM"));
+        $("12").val("");
+        $("12").val(saveInput12);
+
+        var saveInput1 = JSON.parse(localStorage.getItem("1 PM"));
+        $("1").val("");
+        $("1").val(saveInput1);
+
+        var saveInput2 = JSON.parse(localStorage.getItem("2 PM"));
+        $("2").val("");
+        $("2").val(saveInput2);
+
+        var saveInput3 = JSON.parse(localStorage.getItem("3 PM"));
+        $("3").val("");
+        $("3").val(saveInput3);
+
+        var saveInput4 = JSON.parse(localStorage.getItem("4 PM"));
+        $("4").val("");
+        $("4").val(saveInput4);
+
+        var saveInput5 = JSON.parse(localStorage.getItem("5 PM"));
+        $("5").val("");
+        $("5").val(saveInput5);
+    }
+
