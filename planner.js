d6d846df74e7c3ce91d417ddca7a4e09b286d21d
Christian Sanchez <cas81695@gmail.com>
2020-02-01 01:05:14 -0500

    added descriptions on the planner.js functions and index.html

--- planner.js
+++ planner.js
@@ -1,7 +1,12 @@
+// This a constant of the current time in date and hour
+
 const m = moment();
+
+// This shortcut allows the current date variable in the index to be the result of the moment function
 
 $("#currentDate").text(m.format("dddd, MMMM Do"));
 
+// This shortcut sets the functions to activated in a set order
 
 $(document).ready(function() {
 
@@ -10,21 +15,34 @@ $(document).ready(function() {
 
 });
 
+// The function allows for the hour blocks to change from the current time
+
 function hourColor () {
 
+ // The current time is detemined by the constant moment with changing variable of the hours   
     var currentTime = moment().hours();
+
+ // This shortcut allows the variable of the hour block to be an interger that is compared to the value of the current time 
+ // These numbers determine the color of the blocks
 
     $(".input").each(function() {
         var hourBlock = parseInt($(this).attr("id"));
+// This determines that is the hour block is past the current time, it will change the block to the specification of ".past" in style.css
 
         if (currentTime > hourBlock) {
             $(this).addClass("past");
             $(this).removeClass("future");
             $(this).removeClass("present");
+
+// This determines that is the hour block is ahead the current time, it will change the block to the specification of ".future" in style.css
+
         } else if (currentTime < hourBlock) {
             $(this).addClass("future");
             $(this).removeClass("present");
             $(this).removeClass("past");
+
+// This determines that is the hour block is the same as the current time, it will change the block to the specification of ".present" in style.css
+
         } else {
             $(this).addClass("present");
             $(this).removeClass("past");
@@ -33,10 +51,15 @@ function hourColor () {
         });
     }
 
+// This variable for the text
 
 var info;
 
+// This variable for the textbox
+
 var hourText;
+
+// This shortcut allows the save button to inciate a function that places the info in a local storage
 
     $(".saveButton").click(function() {
         info = $(this).siblings(".input").val();
@@ -47,7 +70,8 @@ var hourText;
         inputText ();
     
     })
-
+// This function allows each hour blocks text to placed in a local storage that refers to the previous shortcut function 
+    
     function inputText () {
         var saveInput9 = JSON.parse(localStorage.getItem("9 AM"));
         $("#9").val("");
